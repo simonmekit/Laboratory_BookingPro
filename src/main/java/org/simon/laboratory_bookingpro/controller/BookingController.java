071d373a4d5fd316e02eca78667bf7559bab6594
simon mekit <simonmekit@gmail.com>
2024-03-22 12:03:50 -0400

    nav menu with bootstrap

--- src/main/java/org/simon/laboratory_bookingpro/controller/BookingController.java
+++ src/main/java/org/simon/laboratory_bookingpro/controller/BookingController.java
@@ -1,15 +1,17 @@
 package org.simon.laboratory_bookingpro.controller;
 
+import org.simon.laboratory_bookingpro.dto.Booking;
+import org.simon.laboratory_bookingpro.dto.LabLocation;
+import org.simon.laboratory_bookingpro.dto.UserDto;
 import org.simon.laboratory_bookingpro.model.User;
 import org.simon.laboratory_bookingpro.repositoryservice.BookingService;
 import org.simon.laboratory_bookingpro.repositoryservice.UserService;
-import org.simon.laboratory_bookingpro.service.BookingServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-
-import org.simon.laboratory_bookingpro.dto.UserDto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class BookingController {
@@ -17,31 +19,57 @@ public class BookingController {
     private final BookingService bookingService;
     private final UserService userService;
 
-@Autowired
-    public BookingController(BookingService bookingService, UserService userService){
+    @Autowired
+    public BookingController(BookingService bookingService, UserService userService) {
         this.bookingService = bookingService;
         this.userService = userService;
     }
 
     @GetMapping("/booking")
-    public String createBooking(Model model){
-    model.addAttribute("labUser", new UserDto());
+    public String createBooking(Model model) {
+        model.addAttribute("labUser", new UserDto());
+        model.addAttribute("booking", new Booking());
         return "createBook";
     }
+
+    @PostMapping("/findUser")
+    public String findUser(@ModelAttribute User user, Model model) {
+        UserDto myUser = userService.findUserByEmail(user.getEmail());
+
+        System.out.println(myUser.toString());
+
+//    String message = null;
+//    if (myUser != null){
+//        message = "User found";
+//        model.addAttribute("laboratoryUser", myUser);
+//        model.addAttribute("message", message);
+//    }
+//    else {
+//        message = "User not found!";
+//        model.addAttribute("laboratoryUser", new User());
+//        model.addAttribute("message", message);
+//    }
+        Booking booking = new Booking();
+        booking.setLabUserDto(myUser);
+        model.addAttribute("booking", booking);
+        model.addAttribute("labUser", new UserDto());
+
+        return "createBook";
+    }
+
     @PostMapping("/saveBooking")
-    public String saveBooking(@ModelAttribute User user, Model model){
-    UserDto myUser = userService.findUserByPhone(user.getPhoneNumber());
-    String message = null;
-    if (myUser != null){
-        message = "User found";
-        model.addAttribute("laboratoryUser", myUser);
-        model.addAttribute("message", message);
+    public String saveBooking(@ModelAttribute Booking booking, Model model) {
+
+        System.out.println(booking.toString());
+
+        LabLocation location = new LabLocation();
+        location.setName(booking.getLabLocation().toString());
+        booking.setLabLocation(location);
+        bookingService.createBooking(booking);
+//            String message = null;
+//            model.addAttribute("message", message);
+
+        return "index";
     }
-    else {
-        message = "User not found!";
-        model.addAttribute("laboratoryUser", new User());
-        model.addAttribute("message", message);
-    }
-    return "createBook";
-    }
+
 }
